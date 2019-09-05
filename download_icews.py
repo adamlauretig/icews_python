2fadf1ec499de1fd54e02a43edd62fa7c46ec415
adamlauretig <lauretig.1@buckeyemail.osu.edu>
2019-09-04 22:35:17 -0400

    getting cameo codes to work

--- download_icews.py
+++ download_icews.py
@@ -2,6 +2,22 @@ import urllib.request
 import requests, zipfile, io
 import pandas as pd
 
+CAMEO_codefile = "CAMEO_codefile.txt"  # translates event text to CAMEO event codes
+CAMEO_eventcodes = {}
+fin = open(CAMEO_codefile,'r') 
+caseno = 1
+line = fin.readline()
+while len(line) > 0:
+    if line.startswith('LABEL'):
+        part = line[line.find(' ')+1:].partition(' ') # split text on space, w/ text after "LABEL: "
+        CAMEO_eventcodes[part[2][:-1]] = part[0][:-1] # assign to dict
+        print(CAMEO_eventcodes[part[2][:-1]])
+        caseno += 1
+    #	if caseno > 32: break   # debugging exit 		
+    line = fin.readline()
+
+
+
 
 urls_to_download = ['https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/28075/WNOBVV']
 r = requests.get(urls_to_download[0])
